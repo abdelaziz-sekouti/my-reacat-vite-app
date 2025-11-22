c824258002047db2c8a9595c21ae49ef206a096b
abdelaziz-sekouti <abdelaziz-sekouti@hotmail.com>
2025-11-22 21:52:48 +0100

    vite.config.js modif

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-reacat-vite-app/'
+  base: '/my-reacat-vite-app/',
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor'
+            if (id.includes('lodash')) return 'lodash'
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
