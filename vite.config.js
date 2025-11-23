d6478fe7dc9ea921e909c5ebf445a08ececc028f
abdelaziz-sekouti <abdelaziz-sekouti@hotmail.com>
2025-11-23 13:27:12 +0100

    package.json and vite.config modif

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-reacat-vite-app/',
-   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor'
-            if (id.includes('lodash')) return 'lodash'
-            return 'vendor'
-          }
-        }
-      }
-    },
-    // increase limit (kb) or set to Infinity to disable warning
-    chunkSizeWarningLimit: 600
-  }
+  base: '/my_react_vite_app/',
+  
 })
